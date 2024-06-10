0b9713fb5daed8dbc7edede559bc39bf26aa3022
Bernard PH1M <ph1m@amsat.org>
2024-06-10 22:00:14 +0200

    KAN-1 Intial setup

--- src/main.h
+++ src/main.h
@@ -1,1 +1,10 @@
-
+/**
+ * @file      main.c
+ *
+ * @brief     Main header
+ * @author    Bernard PH1M
+ * @version   v0.1
+ * @date      2024-06-10
+ * 
+ * HAM radio, when everything else fails. Never stop chasing!
+ */
--- tests/gns/ublox_test.c
+++ tests/gns/ublox_test.c
@@ -1,12 +1,10 @@
 /**
- * @file      main.c
+ * @file      ublox_test.c
  *
- * @brief     Main file
+ * @brief     Ublox NEO 9M test file
  * @author    Bernard PH1M
  * @version   v0.1
  * @date      2024-06-10
  * 
  * HAM radio, when everything else fails. Never stop chasing!
  */
-
-#include "main.h"
